b04ac762d2c7ece4fcd2ab55d36354c913ca621d
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2024-01-15 15:58:32 +0300

    Add error page component

---
+++ src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-col items-center justify-center h-screen">
+	<div class="text-center text-4xl font-bold text-sys-on-primary font-serif">
+		<h1 class=" mb-4">404</h1>
+		<p class="text-lg">Page Not Found</p>
+	</div>
+</div>
